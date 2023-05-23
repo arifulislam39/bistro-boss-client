0e6988149a96559d35c505f15323699098c79e77
ariful-islam <arifulislam204909@gmail.com>
2023-05-24 01:00:25 +0600

    ..

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,12 +3,13 @@ import NavBar from "../Pages/Shared/Navbar/NavBar";
 import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div><NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 
 const Home = () => {
     return (
         <div>
-            <h3>this is home</h3>
+            <Banner></Banner>
+            <Category></Category>
+           
         </div>
     );
 };
